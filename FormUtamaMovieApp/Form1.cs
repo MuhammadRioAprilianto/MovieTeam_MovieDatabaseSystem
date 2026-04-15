ea96d895feb9c5120531de4bdf50ac32e183eb56
MuhammadRioAprilianto <rma994636@gmail.com>
2026-04-15 13:01:14 +0700

    fungsi untuk memuat halaman ke dalam panel (TampilkanHalaman), dan pengaturan awal saat aplikasi dibuka.

--- FormUtamaMovieApp/Form1.cs
+++ FormUtamaMovieApp/Form1.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormUtamaMovieApp
+{
+    public partial class FormUtama : Form
+    {
+        public FormUtama()
+        {
+            InitializeComponent();
+        }
+
+        private void FormUtama_Load(object sender, EventArgs e)
+        {
+            panelSidebar.Visible = false;
+            TampilkanHalaman(new UC_KatalogMovie());
+        }
+
+        public void TampilkanHalaman(UserControl halamanBaru)
+        {
+            panelContent.Controls.Clear();
+            halamanBaru.Dock = DockStyle.Fill;
+            panelContent.Controls.Add(halamanBaru);
+            halamanBaru.BringToFront();
+        }
+
+        private void btnReview_Click(object sender, EventArgs e) => TampilkanHalaman(new UC_Review());
+        private void btnHistory_Click(object sender, EventArgs e) => TampilkanHalaman(new UC_History());
+    }
+}
