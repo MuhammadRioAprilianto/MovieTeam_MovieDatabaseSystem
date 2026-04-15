99822cee46d4318681995ba4e3f8fc7c13854dbb
MuhammadRioAprilianto <rma994636@gmail.com>
2026-04-15 13:15:18 +0700

    Membuat koneksi ke database

--- FormUtamaMovieApp/KoneksiDB.cs
+++ FormUtamaMovieApp/KoneksiDB.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using System.Data.SqlClient;
+
+namespace FormUtamaMovieApp
+{
+    public class KoneksiDB
+    {
+        private static string connString = @"Server=RIZOO\MUHAMMADRIO;Database=MovieDB;Integrated Security=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connString);
+        }
+    }
+
+    // Class SesiUser untuk menyimpan informasi user yang sedang login
+    public static class SesiUser
+    {
+        public static int IdUser { get; set; } = 0;
+        public static string Nama { get; set; } = "";
+        public static int RoleId { get; set; } = 0;
+    }
+}
