efd64e50c97e4a5756d36b3921c909dfddfd0d6b
MuhammadRioAprilianto <rma994636@gmail.com>
2026-04-15 13:09:12 +0700

    Bagian ini menangani penerimaan data film dari halaman katalog dan memastikan data yang dimasukkan user sudah sesuai sebelum dikirim ke database.

--- FormUtamaMovieApp/FormIsiReview.cs
+++ FormUtamaMovieApp/FormIsiReview.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace FormUtamaMovieApp
+{
+    public partial class FormIsiReview : Form
+    {
+        private int idMoviePilihan;
+
+        public FormIsiReview(int id, string judul)
+        {
+            InitializeComponent();
+            this.idMoviePilihan = id;
+            lblJudulFilm.Text = "Memberi ulasan untuk: " + judul;
+
+            btnSimpan.Click += btnSimpan_Click;
+            btnBatal.Click += btnBatal_Click;
+        }
+
+        private void btnBatal_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
