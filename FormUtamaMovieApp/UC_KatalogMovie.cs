b72c405b3e8409b47dc4f1f4ea392beaa8d0a3c0
MuhammadRioAprilianto <rma994636@gmail.com>
2026-04-15 13:22:12 +0700

    aplikasi menarik data film dari database, lengkap dengan fitur pencarian judul menggunakan parameter agar aplikasi aman dari serangan SQL Injection.

--- FormUtamaMovieApp/UC_KatalogMovie.cs
+++ FormUtamaMovieApp/UC_KatalogMovie.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace FormUtamaMovieApp
+{
+    public partial class UC_KatalogMovie : UserControl
+    {
+        public UC_KatalogMovie()
+        {
+            InitializeComponent();
+            LoadDataKatalog();
+        }
+
+        private void UC_KatalogMovie_Load(object sender, EventArgs e)
+        {
+            LoadDataKatalog();
+        }
+
+        public void LoadDataKatalog(string keyword = "")
+        {
+            flpKatalogMovie.Controls.Clear();
+
+            using (SqlConnection conn = KoneksiDB.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT movie_id, judul FROM Movies WHERE is_deleted = 0";
+
+                    if (!string.IsNullOrEmpty(keyword))
+                    {
+                        query += " AND judul LIKE @keyword";
+                    }
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        if (!string.IsNullOrEmpty(keyword))
+                        {
+                            cmd.Parameters.AddWithValue("@keyword", "%" + keyword + "%");
+                        }
