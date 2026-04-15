52c77535b35ea39f8945aed873efd10d159c74c1
MuhammadRioAprilianto <rma994636@gmail.com>
2026-04-15 13:12:58 +0700

    pengaturan tampilan awal, seperti menyembunyikan password dan logika perpindahan layar antara panel Login dan panel Register tanpa harus membuka form baru.

--- FormUtamaMovieApp/FormLogin.cs
+++ FormUtamaMovieApp/FormLogin.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace FormUtamaMovieApp
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+
+            txtPasswordLogin.UseSystemPasswordChar = true;
+            txtPasswordRegis.UseSystemPasswordChar = true;
+
+            panelLogin.Visible = true;
+            panel1.Visible = false;
+
+            linkToRegister.LinkClicked += linkToRegister_LinkClicked;
+            RegisterToLogin.LinkClicked += RegisterToLogin_LinkClicked;
+        }
+
+        private void linkToRegister_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            panel1.Visible = true;
+            panelLogin.Visible = false;
+        }
+
+        private void RegisterToLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            panelLogin.Visible = true;
+            panel1.Visible = false;
+        }
