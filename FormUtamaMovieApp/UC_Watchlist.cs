a2433c3ff117db8ee1f83c5c7e67f52657ff0684
MuhammadRioAprilianto <rma994636@gmail.com>
2026-04-15 13:49:33 +0700

    Bagian ini fokus pada pembersihan layar dan pengecekan apakah ada user yang sedang login. Jika tidak ada, sistem akan memberikan pesan peringatan langsung di dalam panel.

--- FormUtamaMovieApp/UC_Watchlist.cs
+++ FormUtamaMovieApp/UC_Watchlist.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace FormUtamaMovieApp
+{
+    public partial class UC_Watchlist : UserControl
+    {
+        public UC_Watchlist()
+        {
+            InitializeComponent();
+            LoadDataWatchlist();
+        }
+
+        public void LoadDataWatchlist()
+        {
+            flpWatchlist.Controls.Clear();
+
+            if (SesiUser.IdUser == 0)
+            {
+                Label lblPesan = new Label();
+                lblPesan.Text = "Silakan login untuk melihat Watchlist Anda.";
+                lblPesan.AutoSize = true;
+                lblPesan.ForeColor = System.Drawing.Color.White;
+                flpWatchlist.Controls.Add(lblPesan);
+                return;
+            }
