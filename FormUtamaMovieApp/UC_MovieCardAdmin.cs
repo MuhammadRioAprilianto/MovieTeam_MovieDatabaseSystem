62657ee5516d10228f5854a1208254f50103362f
MuhammadRioAprilianto <rma994636@gmail.com>
2026-04-15 13:26:47 +0700

    Bagian ini fokus pada penerimaan data dari parent (SetData) dan logika untuk memicu proses edit dengan memanggil form dialog.

--- FormUtamaMovieApp/UC_MovieCardAdmin.cs
+++ FormUtamaMovieApp/UC_MovieCardAdmin.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace FormUtamaMovieApp
+{
+    public partial class UC_MovieCardAdmin : UserControl
+    {
+        private int _movieId;
+        private string _judul;
+
+        public UC_MovieCardAdmin()
+        {
+            InitializeComponent();
+        }
+
+        public void SetData(int id, string judul)
+        {
+            _movieId = id;
+            _judul = judul;
+            lblJudulMovie.Text = judul;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            FormDialogMovie frm = new FormDialogMovie(_movieId);
+
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                if (this.ParentForm is FormUtama mainForm)
+                {
+                    mainForm.TampilkanHalaman(new UC_ManageMovie());
+                }
+            }
+        }
