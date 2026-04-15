f0958a2eccd92991f5a1b7481dec3c49e6399255
MuhammadRioAprilianto <rma994636@gmail.com>
2026-04-15 13:50:15 +0700

    Bagian ini menangani pengambilan data film yang spesifik dipilih oleh user tersebut dan menampilkannya kembali dalam bentuk kartu film (UC_MovieItem).

--- FormUtamaMovieApp/UC_Watchlist.cs
+++ FormUtamaMovieApp/UC_Watchlist.cs
@@ -25,3 +25,46 @@ namespace FormUtamaMovieApp
                 flpWatchlist.Controls.Add(lblPesan);
                 return;
             }
+
+            using (SqlConnection conn = KoneksiDB.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"
+                        SELECT m.movie_id, m.judul 
+                        FROM Watchlists w
+                        INNER JOIN Movies m ON w.movie_id = m.movie_id
+                        WHERE w.user_id = @uid";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@uid", SesiUser.IdUser);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                UC_MovieItem kartuFilm = new UC_MovieItem();
+                                int id = Convert.ToInt32(reader["movie_id"]);
+                                string judul = reader["judul"].ToString();
+
+                                kartuFilm.SetDataFilm(id, judul);
+
+                                kartuFilm.Width = 160;
+                                kartuFilm.Height = 240;
+                                kartuFilm.Margin = new Padding(10);
+
+                                flpWatchlist.Controls.Add(kartuFilm);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gagal memuat Watchlist: " + ex.Message, "Error");
+                }
+            }
+        }
+    }
+}
