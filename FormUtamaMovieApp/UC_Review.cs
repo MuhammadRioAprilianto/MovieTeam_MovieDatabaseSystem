808b8f3a30ee5a91c4abdd67dfcc0a0cc1975446
MuhammadRioAprilianto <rma994636@gmail.com>
2026-05-11 22:05:47 +0700

    Mengubah select data review menjadi menggunakan view

--- FormUtamaMovieApp/UC_Review.cs
+++ FormUtamaMovieApp/UC_Review.cs
@@ -26,17 +26,7 @@ namespace FormUtamaMovieApp
                 try
                 {
                     conn.Open();
-                    string query = @"
-                        SELECT 
-                            r.review_id, 
-                            m.judul AS 'Judul Film', 
-                            r.skor_rating AS 'Rating', 
-                            r.komentar AS 'Ulasan / Komentar', 
-                            r.tanggal_diposting AS 'Tanggal' 
-                        FROM Reviews r
-                        INNER JOIN Movies m ON r.movie_id = m.movie_id
-                        WHERE r.user_id = @uid
-                        ORDER BY r.tanggal_diposting DESC";
+                    string query = "SELECT * FROM vwReview WHERE user_id = @uid ORDER BY Tanggal DESC";
 
                     using (SqlCommand cmd = new SqlCommand(query, conn))
                     {
@@ -48,18 +38,13 @@ namespace FormUtamaMovieApp
 
                         dgvComment.DataSource = dt;
 
-                        if (dgvComment.Columns["review_id"] != null)
-                            dgvComment.Columns["review_id"].Visible = false;
+                        if (dgvComment.Columns["review_id"] != null) dgvComment.Columns["review_id"].Visible = false;
+                        if (dgvComment.Columns["user_id"] != null) dgvComment.Columns["user_id"].Visible = false;
 
                         dgvComment.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-                        dgvComment.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
-                        dgvComment.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
                     }
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Gagal memuat: " + ex.Message);
-                }
+                catch (Exception ex) { MessageBox.Show("Gagal memuat: " + ex.Message); }
             }
         }
 
