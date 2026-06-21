3dcb11339e958b172b5e9042808377fa5cddecda
MuhammadRioAprilianto <rma994636@gmail.com>
2026-06-21 12:24:23 +0700

    membuat class data review

---
+++ FormUtamaMovieApp/DataReview.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormUtamaMovieApp
+{
+    class DataReview
+    {
+        public string Waktu { get; set; }
+        public string User { get; set; }
+        public string Film { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+    }
+}
