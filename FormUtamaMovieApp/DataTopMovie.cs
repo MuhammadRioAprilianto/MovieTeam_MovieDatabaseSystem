b45ef8687c6c2cf92410bcfab1967f31bc0a5355
MuhammadRioAprilianto <rma994636@gmail.com>
2026-06-21 12:25:03 +0700

    membuat class TopMovie

---
+++ FormUtamaMovieApp/DataTopMovie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormUtamaMovieApp
+{
+    class DataTopMovie
+    {
+        public string Judul { get; set; }
+        public int TotalSimpan { get; set; }
+    }
+}
