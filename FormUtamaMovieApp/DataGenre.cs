c21b770466615a7ba1e00ad9d641ceb0b50504ef
MuhammadRioAprilianto <rma994636@gmail.com>
2026-06-21 12:23:58 +0700

    membuat class data genre

---
+++ FormUtamaMovieApp/DataGenre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormUtamaMovieApp
+{
+    class DataGenre
+    {
+        public string Genre { get; set; }
+        public int Peminat { get; set; }
+    }
+}
